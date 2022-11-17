dfcd7df88b4027764d53900c022ab14b23a3fe04
TimmyLi <artelaw@gmail.com>
2022-11-17 23:22:57 +0600

    Тестирование Postman позитивный результат

---
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+
+class PostmanTest {
+    @Test
+    void PostRequest() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
